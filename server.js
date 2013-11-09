c2ce73de823eaf056172b21af067a5cabb4b3cf2
gabriel_lyons@brown.edu <gabe@Gabes-MacBook-Air.local>
2013-11-08 19:59:48 -0500

    hacky db stuff

--- server.js
+++ server.js
@@ -2,6 +2,94 @@ var express = require("express");
 var dropbox = require("dropbox");
 var fs = require("fs");
 var https = require("https");
+var crypto = require('crypto');
+request = require('request'),
+        url = require('url');
+
+var client = new Dropbox.Client({
+    key: "dfxzvgtw5vbh0r4",
+    secret: "y3fpf3zgcpxecpr"
+});
+
+
+
+client.authDriver(new Dropbox.AuthDriver.Popup({
+    receiverUrl: "https://fdkfjskdlfjasdklfj/oauth_receiver.html"}));
+client.authDriver(new Dropbox.AuthDriver.NodeServer(8191));
+
+// insert your app key and secret here
+var APP_KEY = 'dfxzvgtw5vbh0r4'
+var APP_SECRET = 'y3fpf3zgcpxecpr';
+
+function generateCSRFToken() {
+        return crypto.randomBytes(18).toString('base64')
+                .replace(/\//g, '-').replace(/\+/g, '_');
+}
+
+function generateRedirectURI(req) {
+        return url.format({
+                        protocol: req.protocol,
+                        host: req.headers.host,
+                        pathname: app.path() + '/callback'
+        });
+}
+
+app.get('/', function (req, res) {
+        var csrfToken = generateCSRFToken();
+        res.cookie('csrf', csrfToken);
+        res.redirect(url.format({
+                protocol: 'https',
+                hostname: 'www.dropbox.com',
+                pathname: '1/oauth2/authorize',
+                query: {
+                        client_id: APP_KEY,
+                        response_type: 'code',
+                        state: csrfToken,
+                        redirect_uri: generateRedirectURI(req)
+                }
+        }));
+});
+
+app.get('/callback', function (req, res) {
+        if (req.query.error) {
+                return res.send('ERROR ' + req.query.error + ': ' + req.query.error_description);
+        }
+
+        // check CSRF token
+        if (req.query.state !== req.cookies.csrf) {
+                return res.status(401).send(
+                        'CSRF token mismatch, possible cross-site request forgery attempt.'
+                );
+        }
+        // exchange access code for bearer token
+        request.post('https://api.dropbox.com/1/oauth2/token', {
+                form: {
+                        code: req.query.code,
+                        grant_type: 'authorization_code',
+                        redirect_uri: generateRedirectURI(req)
+                },
+                auth: {
+                        user: APP_KEY,
+                        pass: APP_SECRET
+                }
+        }, function (error, response, body) {
+                var data = JSON.parse(body);
+
+                if (data.error) {
+                        return res.send('ERROR: ' + data.error);
+                }
+
+                // extract bearer token
+                var token = data.access_token;
+
+                // use the bearer token to make API calls
+                request.get('https://api.dropbox.com/1/account/info', {
+                        headers: { Authorization: 'Bearer ' + token }
+                }, function (error, response, body) {
+                        res.send('Logged in successfully as ' + JSON.parse(body).display_name + '.');
+                });
+        });
+});
 
 var privateKey = fs.readFileSync("ssl/gabes-key.pem", "utf8");
 var certificate = fs.readFileSync("ssl/gabes-cert.pem", "utf8");
